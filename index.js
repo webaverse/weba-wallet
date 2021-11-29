9c83a39595b26680ff6f504762594710746bb157
mujabmuneeb <mujabmuneeb@gmail.com>
2021-11-29 05:23:07 +0500

    secure

--- index.js
+++ index.js
@@ -5,35 +5,43 @@ let express = require('express'),
 
 var CryptoJS = require("crypto-js");
 const { uuid } = require('uuidv4');
+var { createStore } = require('./storage-module/build/secure-storage');
 
-var dynamo = require('dynamodb');
-var Joi = require('joi');
-var config = require('./aws-config.json');
+const store = createStore()
+
+// store.saveKey('login-token', '123', 'password', { privateKey: 'token' });
+// const { privateKey } = store.getPrivateKeyData('login-token', 'password', value);
+// if(!privateKey) {
+//   return resp.send({'Error': 'Decryption failed. Wrong password.'});
+// }
+// var dynamo = require('dynamodb');
+// var Joi = require('joi');
+// var config = require('./aws-config.json');
 
 // check if config file exists
-if(config) {
-  dynamo.AWS.config.update({
-    "accessKeyId":config.accessKeyId,
-    "secretAccessKey":config.secretAccessKey,
-    "region":config.region
-  });
-}
-else {
-  throw new Error;
-}
+// if(config) {
+//   dynamo.AWS.config.update({
+//     "accessKeyId":config.accessKeyId,
+//     "secretAccessKey":config.secretAccessKey,
+//     "region":config.region
+//   });
+// }
+// else {
+//   throw new Error;
+// }
 
-var WebaWallet = dynamo.define(config.tableName, {
-  hashKey : 'mnemonic',
+// var WebaWallet = dynamo.define(config.tableName, {
+//   hashKey : 'mnemonic',
  
-  // add the timestamp attributes (updatedAt, createdAt)
-  timestamps : true,
+//   // add the timestamp attributes (updatedAt, createdAt)
+//   timestamps : true,
  
-  schema : {
-    mnemonic   : Joi.string(),
-    password    : Joi.string(),
-    data: Joi.object()
-  }
-});
+//   schema : {
+//     mnemonic   : Joi.string(),
+//     password    : Joi.string(),
+//     data: Joi.object()
+//   }
+// });
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,131 +50,80 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
  
-function encrypt(val, secret) {
-  var cipher = CryptoJS.AES.encrypt(val, secret);
-  cipher = cipher.toString();
-  return cipher;
-}
-
-function decrypt(val, secret) {
-  var decipher = CryptoJS.AES.decrypt(val, secret);
-  decipher = decipher.toString(CryptoJS.enc.Utf8);
-  return decipher;
-}
-
-function setPassword(mnemonic, password) {
-  return new Promise(async (resolve, reject) => {
-
-    var en_pw = encrypt(password, mnemonic); // encrypt password
-
-    WebaWallet.update({mnemonic: mnemonic, password: en_pw, data: {'temp-key': 'temp-password'}}, async function (err, res) {
-      if(err) {
-        return reject(err);
-      }
-      else {
-        return resolve(res);
-      }
-    });
-  })
-}
+// function encrypt(val, secret) {
+//   var cipher = CryptoJS.AES.encrypt(val, secret);
+//   cipher = cipher.toString();
+//   return cipher;
+// }
+// function decrypt(val, secret) {
+//   var decipher = CryptoJS.AES.decrypt(val, secret);
+//   decipher = decipher.toString(CryptoJS.enc.Utf8);
+//   return decipher;
+// }
+// function setPassword(mnemonic, password) {
+//   return new Promise(async (resolve, reject) => {
+//     var en_pw = encrypt(password, mnemonic); // encrypt password
+//     WebaWallet.update({mnemonic: mnemonic, password: en_pw, data: {'temp-key': 'temp-password'}}, async function (err, res) {
+//       if(err) {
+//         return reject(err);
+//       }
+//       else {
+//         return resolve(res);
+//       }
+//     });
+//   })
+// }
 
 app.post('/save-data', function(req, resp) {
 
-  // validate token, temp_token and key-value data
-  if(req.body.mnemonic && req.body.data) {
-    var mnemonic = req.body.mnemonic;
-    var newData = req.body.data;
+  if(req.body.key && req.body.value) {
+    var {key, value} = req.body;
   }
   else {
-    return resp.send({'Error': 'Invalid mnemonic or Key-Value data'})
+    return resp.send({'Error': 'Invalid data'})
   }
 
-  mnemonic = decrypt(mnemonic, config.encryptionKey);
-
-  // fetch record from dynamoDB
-  WebaWallet.get(mnemonic, async function (err, res) {
-
-    if(res) {
-      var obj = res.attrs;
-
-      // validate temp_token
-      if(obj.mnemonic == mnemonic && decrypt(obj.password, mnemonic)) {
-        // store new key-value data
-        obj.data[newData.key] = newData.value
-        WebaWallet.update({mnemonic: mnemonic, data: obj.data}, async function (err, res) {
-          if(err) {
-            return resp.send({'Error': JSON.stringify(err)});
-          }
-          else {
-            return resp.send({'Success': 'New key value saved', 'data': obj.data});
-          }
-        });
-      }
-      else {
-        return resp.send({'Error': 'Invalid Temp Token, Login again to get a fresh-one'});
-      }
-    }
-    // check for error
-    else {
-      return resp.send({'Error': err});
-    }
+  var result = store.saveKey(key, 'password', { privateKey: value });
+  result.then((data)=>{
+    return resp.send({'Success': 'New key value saved', 'data': data});
   })
+  .catch((err)=>{
+    return resp.send({'Error': err});
+  })
+
 
 });
 
 app.post('/get-keys', function(req, resp) {
 
-  // validate token, temp_token and key-value data
-  if(req.body.mnemonic) {
-    var mnemonic = req.body.mnemonic;
+  var result = {};
+  if(req.body.key.length) {
+    const { privateKey } = store.getPrivateKeyData(key, 'password', JSON.parse(req.body.value)[req.body.key])
+    result[req.body.key] = privateKey;
   }
   else {
-    return resp.send({'Error': 'Invalid Token or Temp token'})
+    for (const [key, value] of Object.entries(JSON.parse(req.body.value))) {
+      const { privateKey } = store.getPrivateKeyData(req.body.key, 'password', value)
+      result[key] = privateKey;
+    }
   }
-
-  // fetch data from dynamoDB
-  WebaWallet.get(mnemonic, async function (err, res) {
-    if(res) {
-      var obj = res.attrs;
-
-      if(obj.password) {
-        // validate fresh temp_token
-        if(obj.mnemonic == mnemonic && decrypt(obj.password, mnemonic)) {
-          var en_mnemonic = encrypt(mnemonic, config.encryptionKey); // encrypt mnemonic
-          // return user data
-          return resp.send({'Success': 'Keys fetched', 'data': obj.data, 'mnemonic': en_mnemonic});
-        }
-        else {
-          return resp.send({'Error': 'Invalid mnemonic, Login again to get a fresh-one'});
-        }
-      }
-      else {
-        var en_mnemonic = encrypt(mnemonic, config.encryptionKey); // encrypt mnemonic
-        return resp.send({'Message': 'Password', 'mnemonic': en_mnemonic});
-      }
-    }
-    // check for error
-    else {
-      return resp.send({'Error': err});
-    }
-  })
-
+  return resp.send({'Data': result});
 });
 
-app.post('/validate', async function(req, resp) {
+// app.post('/validate', async function(req, resp) {
     
-  if(req.body.mnemonic && req.body.password) {
-    var {mnemonic, password} = req.body;
-  }
-  else {
-    return resp.send({'Error': 'Invalid mnemonic or Password'})
-  }
+//   if(req.body.mnemonic && req.body.password) {
+//     var {mnemonic, password} = req.body;
+//   }
+//   else {
+//     return resp.send({'Error': 'Invalid mnemonic or Password'})
+//   }
 
-  var dc_mnemonic = decrypt(mnemonic, config.encryptionKey)
-  var result = await setPassword(dc_mnemonic, password);
-  var en_mnemonic = encrypt(result.attrs.mnemonic, config.encryptionKey)
-  return resp.send({'Success': 'User password updated', 'mnemonic': en_mnemonic});
-});
+//   var dc_mnemonic = decrypt(mnemonic, config.encryptionKey)
+//   var result = await setPassword(dc_mnemonic, password);
+//   var en_mnemonic = encrypt(result.attrs.mnemonic, config.encryptionKey)
+//   return resp.send({'Success': 'User password updated', 'mnemonic': en_mnemonic});
+// });
 
 // Create port
 
--- public/script.js
+++ public/script.js
@@ -1,43 +1,46 @@
-$(document).ready(function() { 
-  window.opener.postMessage('received', 'app.webaverse.com');
- });
+// saveData('mujabs', 'muneeb');
+getKeys('');
 
-window.addEventListener("message", receiveMessage, false);
-var mnemonic = '';
-var source = '';
-var origin = '';
+// $(document).ready(function() { 
+//   window.opener.postMessage('received', 'http://localhost:3000');
+//  });
 
-function receiveMessage(event){
-if (event.origin !== 'app.webaverse.com')
-    return;
+// window.addEventListener("message", receiveMessage, false);
+// // var mnemonic = '';
+// var source = '';
+// var origin = '';
 
-    source = event.source;
-    origin = event.origin;
+// function receiveMessage(event){
+// if (event.origin !== 'http://localhost:3000')
+//     return;
 
-    if(JSON.parse(event.data).mnemonic) {
+//     source = event.source;
+//     origin = event.origin;
 
-      mnemonic = JSON.parse(event.data).mnemonic;
+//     // if(JSON.parse(event.data).mnemonic) {
 
-      if(JSON.parse(event.data).data) {
-        saveData(mnemonic, JSON.parse(event.data).data)
-      }
-      else {
-        getKeys(mnemonic);
-      }
-    }
-    else if(JSON.parse(event.data).p_mnemonic) {
-        mnemonic = JSON.parse(event.data).p_mnemonic;
-    }
-    else {
-        return source.postMessage({"Error": "Invalid mnemonic"}, origin);
-    }
+//     //   mnemonic = JSON.parse(event.data).mnemonic;
 
-}
+//     //   if(JSON.parse(event.data).data) {
+//     //     saveData(mnemonic, JSON.parse(event.data).data)
+//     //   }
+//     //   else {
+//     //     getKeys(mnemonic);
+//     //   }
+//     // }
+//     // else if(JSON.parse(event.data).p_mnemonic) {
+//     //     mnemonic = JSON.parse(event.data).p_mnemonic;
+//     // }
+//     // else {
+//     //     return source.postMessage({"Error": "Invalid mnemonic"}, origin);
+//     // }
 
-function saveData(mnemonic, newData) {
+// }
+
+function saveData(key, value) {
     var data = {
-        mnemonic: mnemonic,
-        data: newData
+        key: key,
+        value: value
     };
 
     fetch('/save-data', {
@@ -49,13 +52,26 @@ function saveData(mnemonic, newData) {
         })
         .then(response => response.json())
         .then(json => {
-            source.postMessage(json, origin);
+            if(json.Success) {
+                var currData = localStorage.getItem('data');
+                if(currData) {
+                    var newData = JSON.parse(currData)[key] = json.data;
+                    localStorage.setItem('data', newData);
+                }
+                else {
+                    var data = {};
+                    data[key] = JSON.parse(json.data)
+                    localStorage.setItem('data', JSON.stringify({data}));
+                }
+            }
+            // source.postMessage(json, origin);
         });
 }
 
-function getKeys(mnemonic) {
+function getKeys(key) {
     var data = {
-        mnemonic: mnemonic,
+        key: key,
+        value: localStorage.getItem('data'),
     };
 
     fetch('/get-keys', {
@@ -67,37 +83,38 @@ function getKeys(mnemonic) {
         })
         .then(response => response.json())
         .then(json => {
-            source.postMessage(json, origin);
+            console.log('json', json);
+            // source.postMessage(json, origin);
         });
 }
 
-function submit() {
+// function submit() {
 
-    var data = {
-        mnemonic: mnemonic,
-        password: document.getElementById("password").value
-    };
+//     var data = {
+//         mnemonic: mnemonic,
+//         password: document.getElementById("password").value
+//     };
 
-    fetch('/validate', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-      }
-    })
-    .then(response => response.json())
-    .then(json => {
-      if(json.Error) {
-        document.getElementById("error").innerHTML = json.Error;
-      }
-      else {
-        document.getElementById("error").innerHTML = "";
-        source.postMessage(json, origin);
-        $( ".lock" ).toggleClass('unlocked');
-      }
-    });
+//     fetch('/validate', {
+//       method: 'POST',
+//       body: JSON.stringify(data),
+//       headers: {
+//           'Content-type': 'application/json; charset=UTF-8'
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(json => {
+//       if(json.Error) {
+//         document.getElementById("error").innerHTML = json.Error;
+//       }
+//       else {
+//         document.getElementById("error").innerHTML = "";
+//         source.postMessage(json, origin);
+//         $( ".lock" ).toggleClass('unlocked');
+//       }
+//     });
 
-}
+// }
 
 function lockedFn() {
     var val = document.getElementById("password").value;
